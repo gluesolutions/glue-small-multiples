8bf19354c505dc19c346fd6bb5e697625c906efa
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2022-09-14 10:28:13 -0400

    Cleanup layer_artist file

--- glue_small_multiples/layer_artist.py
+++ glue_small_multiples/layer_artist.py
@@ -1,101 +1,21 @@
 import numpy as np
 
+from echo import keep_in_sync
+
+from glue.core import BaseData, Subset
+from glue.utils import defer_draw, broadcast_to, ensure_numerical, datetime64_to_mpl
+from glue.core.exceptions import IncompatibleAttribute
+
 from glue.viewers.matplotlib.layer_artist import MatplotlibLayerArtist
 from glue.viewers.scatter.layer_artist import ScatterLayerArtist
 from glue.viewers.scatter.state import ScatterLayerState
 from glue.viewers.scatter.layer_artist import set_mpl_artist_cmap
-
-from glue.core import BaseData, Subset
-
-from echo import keep_in_sync
-from glue.utils import defer_draw, broadcast_to, ensure_numerical, datetime64_to_mpl
-from glue.core.exceptions import IncompatibleAttribute
-
 
 from .utils import PanTrackerMixin
 from .state import SmallMultiplesLayerState, FacetScatterLayerState
 
 __all__ = ['SmallMultiplesLayerArtist', 'FacetScatterLayerArtist']
 
-class SmallMultiplesLayerArtist(MatplotlibLayerArtist, PanTrackerMixin):
-    """
-    We generally want this to be a ScatterLayerArtist, but...
-    
-    It's more like... EACH axes in our list of axes is a ScatterLayerArtist
-    with a different dataset, but all the same state variables.
-    
-    Okay, so then... we just need to call _update_scatter on all the subplots
-    whenever something happens to this LayerArtist that might require a redraw
-    
-    Currently it seems as if the small multiples are not showing the correct data
-    Perhaps they are showing too many points? Or they are showing all the data except
-    what they should be showing.
-    
-    """
-    
-    _layer_state_cls = ScatterLayerState
-
-    def __init__(self, axes, viewer_state, layer_state=None, layer=None):
-        super().__init__(axes, viewer_state, layer_state=layer_state, layer=layer)
-
-        self.axes_subplots = axes
-        self.scatter_layer_artists = []
-        self.scatter_layer_artists_syncs = []
-        #self._viewer_state.layers_data = []
-        self._viewer_state.add_global_callback(self._update_scatter)
-        self.state.add_global_callback(self._update_scatter)
-        #print(f"{self._viewer_state.layers_data=} at __init__") #We have an "extra" layer here, probably because we have this LayerArtist at all
-        #print(f"{self._viewer_state.layers=} at __init__")
-        #_ = self._viewer_state.layers.pop()
-        #print(f"{self._viewer_state.layers=} at __init__")
-
-    def _set_axes(self):
-        
-        for sla in self.scatter_layer_artists:
-            sla.clear()
-            sla.remove()
-        self.scatter_layer_artists = []
-        self.scatter_layer_artists_syncs = []
-        #self._viewer_state.layers_data = []
-
-        flat_axes = self.axes_subplots.flatten()
-        for ax, facet_mask, facet_subset in zip(flat_axes, self._viewer_state.data_facet_masks, self._viewer_state.data_facet_subsets):
-            sla = FacetScatterLayerArtist(ax, self._viewer_state, layer=self.layer, 
-                                        facet_mask=facet_mask, facet_subset=facet_subset, scatter_state = self.state)
-            self.scatter_layer_artists.append(sla)
-            #self.state.add_global_callback(sla._update_scatter)
-            for visual_property in (CMAP_PROPERTIES | MARKER_PROPERTIES | LINE_PROPERTIES):
-                sla_sync = keep_in_sync(self.state, visual_property, sla.state, visual_property)
-                self.scatter_layer_artists_syncs.append(sla_sync)
-            sla._update_scatter(force=True)
-
-
-    @defer_draw
-    def _update_scatter(self, force=False, **kwargs):
-        if (self._viewer_state.x_att is None or
-                self._viewer_state.y_att is None or
-                self._viewer_state.col_facet_att is None or
-                self._viewer_state.reference_data is None or
-                self.state.layer is None):
-            return
-
-        changed = set() if force else self.pop_changed_properties()
-        if force or any(prop in changed for prop in ('col_facet_att','reference_data')):
-            #print(f"Calling _set_axes() with {changed=}")
-            self._set_axes()
-            
-        #for sla in self.scatter_layer_artists:
-        #    sla._update_scatter(force=True)
-        
-    @defer_draw
-    def update(self):
-        self._update_scatter(force=True)
-        self.redraw()
-    
-        
-    def redraw(self):
-        pass # Is this okay?
-        #self.axes.figure.canvas.draw_idle()
 
 CMAP_PROPERTIES = set(['cmap_mode', 'cmap_att', 'cmap_vmin', 'cmap_vmax', 'cmap'])
 MARKER_PROPERTIES = set(['size_mode', 'size_att', 'size_vmin', 'size_vmax', 'size_scaling', 'size', 'fill'])
@@ -109,15 +29,71 @@ DATA_PROPERTIES = set(['layer', 'x_att', 'y_att', 'cmap_mode', 'size_mode', 'den
                        'vector_visible', 'vx_att', 'vy_att', 'vector_arrowhead', 'vector_mode',
                        'vector_origin', 'line_visible', 'markers_visible', 'vector_scaling',
                        'col_facet_att'])
-        
+
+class SmallMultiplesLayerArtist(MatplotlibLayerArtist, PanTrackerMixin):
+    """
+    EACH axes in our list of axes is a ScatterLayerArtist
+    with a different dataset, but all the same state variables.    
+    """
+    
+    _layer_state_cls = ScatterLayerState
+
+    def __init__(self, axes, viewer_state, layer_state=None, layer=None):
+        super().__init__(axes, viewer_state, layer_state=layer_state, layer=layer)
+
+        self.axes_subplots = axes
+        self.scatter_layer_artists = []
+        self.scatter_layer_artists_syncs = []
+        self._viewer_state.add_global_callback(self._update_scatter)
+        self.state.add_global_callback(self._update_scatter)
+
+    def _set_axes(self):
+
+        for sla in self.scatter_layer_artists:
+            sla.clear()
+            sla.remove()
+        self.scatter_layer_artists = []
+        self.scatter_layer_artists_syncs = []
+
+        flat_axes = self.axes_subplots.flatten()
+        for ax, facet_mask, facet_subset in zip(flat_axes, self._viewer_state.data_facet_masks, 
+                                                self._viewer_state.data_facet_subsets):
+            sla = FacetScatterLayerArtist(ax, self._viewer_state, layer=self.layer, 
+                                          facet_mask=facet_mask, facet_subset=facet_subset, 
+                                          scatter_state = self.state)
+            self.scatter_layer_artists.append(sla)
+            for visual_property in (CMAP_PROPERTIES | MARKER_PROPERTIES | LINE_PROPERTIES):
+                sla_sync = keep_in_sync(self.state, visual_property, sla.state, visual_property)
+                self.scatter_layer_artists_syncs.append(sla_sync)
+            sla._update_scatter(force=True)
+
+    @defer_draw
+    def _update_scatter(self, force=False, **kwargs):
+        if (self._viewer_state.x_att is None or
+                self._viewer_state.y_att is None or
+                self._viewer_state.col_facet_att is None or
+                self._viewer_state.reference_data is None or
+                self.state.layer is None):
+            return
+
+        changed = set() if force else self.pop_changed_properties()
+        if force or any(prop in changed for prop in ('col_facet_att','reference_data')):
+            self._set_axes()
+
+    @defer_draw
+    def update(self):
+        self._update_scatter(force=True)
+        self.redraw()
+
+    def redraw(self):
+        pass # Is this okay?
+        #self.axes.figure.canvas.draw_idle()
+
+
 class FacetScatterLayerArtist(ScatterLayerArtist):
     """
-    A custom ScatterLayerArtist that knows how to trim the data appropriately based
-    on a facet_mask
-    
-    Note that because we want to support density maps we also need a custom
-    state function that knows how to trim the data before passing it to make 
-    a 2d histogram.
+    A custom ScatterLayerArtist that knows how to trim the data
+    appropriately based on a facet_mask
     """
     
     _layer_state_cls = FacetScatterLayerState
@@ -135,28 +111,25 @@ class FacetScatterLayerArtist(ScatterLayerArtist):
         self.state.facet_subset = facet_subset
         self.state.title = facet_subset.label
 
-        
     @defer_draw
     def _update_scatter(self, force=False, **kwargs):
-    
-
         if (self._viewer_state.x_att is None or
                 self._viewer_state.y_att is None or
                 self._viewer_state.col_facet_att is None or
                 self._viewer_state.reference_data is None or
                 self.state.layer is None):
             return
-    
+
         changed = set() if force else self.pop_changed_properties()
-    
+
         if force or len(changed & DATA_PROPERTIES) > 0:
             self._update_data()
             force = True
-    
+
         if force or len(changed & VISUAL_PROPERTIES) > 0:
             #print("Visual Attributes Changes...")
             self._update_visual_attributes(changed, force=force)
-            
+
 
     @defer_draw
     def _update_data(self):
@@ -194,7 +167,7 @@ class FacetScatterLayerArtist(ScatterLayerArtist):
             return
         else:
             self.enable()
-    
+
         try:
             if not self.state.density_map:
                 if isinstance(self.layer, Subset): # This is not a very efficient way to do this
@@ -211,13 +184,10 @@ class FacetScatterLayerArtist(ScatterLayerArtist):
             return
         else:
             self.enable()
-        
+
         self.axes.set_title(self.state.title)
-        
+
         if self.state.markers_visible:
-            #print(self.layer)
-            #print(f"{masked_x=}")
-            #print(f"{masked_y=}")
 
             if self.state.density_map:
                 # We don't use x, y here because we actually make use of the
@@ -241,15 +211,12 @@ class FacetScatterLayerArtist(ScatterLayerArtist):
     
     @defer_draw
     def _update_visual_attributes(self, changed, force=False):
-        print(f"Calling **_update_visual_attributes** with {self.layer.label} and {changed=}")
 
         if not self.enabled:
             return
-    
+
         if self.state.markers_visible:
-    
             if self.state.density_map:
-    
                 if self.state.cmap_mode == 'Fixed':
                     if force or 'color' in changed or 'cmap_mode' in changed:
                         self.density_artist.set_color(self.state.color)
@@ -270,11 +237,8 @@ class FacetScatterLayerArtist(ScatterLayerArtist):
                 if force or 'density_contrast' in changed:
                     self.density_auto_limits.contrast = self.state.density_contrast
                     self.density_artist.stale = True
-    
             else:
-    
                 if self._use_plot_artist():
-    
                     if force or 'color' in changed or 'fill' in changed:
                         if self.state.fill:
                             self.plot_artist.set_markeredgecolor('none')
@@ -282,18 +246,18 @@ class FacetScatterLayerArtist(ScatterLayerArtist):
                         else:
                             self.plot_artist.set_markeredgecolor(self.state.color)
                             self.plot_artist.set_markerfacecolor('none')
-    
+
                     if force or 'size' in changed or 'size_scaling' in changed:
                         self.plot_artist.set_markersize(self.state.size *
                                                         self.state.size_scaling)
-    
+
                 else:
-    
+
                     # TEMPORARY: Matplotlib has a bug that causes set_alpha to
                     # change the colors back: https://github.com/matplotlib/matplotlib/issues/8953
                     if 'alpha' in changed:
                         force = True
-    
+
                     if self.state.cmap_mode == 'Fixed':
                         if force or 'color' in changed or 'cmap_mode' in changed or 'fill' in changed:
                             self.scatter_artist.set_array(None)
@@ -315,9 +279,9 @@ class FacetScatterLayerArtist(ScatterLayerArtist):
                             self.scatter_artist.set_edgecolors('none')
                         else:
                             self.scatter_artist.set_facecolors('none')
-    
+
                     if force or any(prop in changed for prop in MARKER_PROPERTIES):
-    
+
                         if self.state.size_mode == 'Fixed':
                             s = self.state.size * self.state.size_scaling
                             s = broadcast_to(s, self.scatter_artist.get_sizes().shape)
@@ -334,7 +298,7 @@ class FacetScatterLayerArtist(ScatterLayerArtist):
                             s *= 0.95
                             s += 0.05
                             s *= (30 * self.state.size_scaling)
-    
+
                         # Note, we need to square here because for scatter, s is actually
                         # proportional to the marker area, not radius.
                         self.scatter_artist.set_sizes(s ** 2)
@@ -342,16 +306,16 @@ class FacetScatterLayerArtist(ScatterLayerArtist):
         for artist in [self.scatter_artist, self.plot_artist,
                        self.vector_artist, self.line_collection,
                        self.density_artist]:
-    
+
             if artist is None:
                 continue
-    
+
             if force or 'alpha' in changed:
                 artist.set_alpha(self.state.alpha)
-    
+
             if force or 'zorder' in changed:
                 artist.set_zorder(self.state.zorder)
-    
+
             if force or 'visible' in changed:
                 # We need to hide the density artist if it is not needed because
                 # otherwise it might still show even if there is no data as the
@@ -368,13 +332,10 @@ class FacetScatterLayerArtist(ScatterLayerArtist):
             self.plot_artist.set_visible(False)
         self.redraw()
 
-
     def compute_density_map(self, *args, **kwargs):
-        print(f"Trying to compute density map with: {','.join(map(str,args))}")
         try:
             density_map = self.state.compute_density_map(*args, **kwargs)
         except IncompatibleAttribute:
-            print("Got an IncompatibleAttribute")
             self.disable_invalid_attributes(self._viewer_state.x_att,
                                             self._viewer_state.y_att)
             return np.array([[np.nan]])
