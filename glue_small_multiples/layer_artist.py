Fix call to removed broadcast_to func

glue_small_multiples/layer_artist.py
@@ -2,7 +2,7 @@ import numpy as np
 from echo import keep_in_sync
 
 from glue.core import BaseData, Subset
-from glue.utils import defer_draw, broadcast_to, ensure_numerical, datetime64_to_mpl
+from glue.utils import defer_draw, ensure_numerical, datetime64_to_mpl
 from glue.core.exceptions import IncompatibleAttribute
 
 from glue.viewers.matplotlib.layer_artist import MatplotlibLayerArtist
@@ -306,7 +306,7 @@ class FacetScatterLayerArtist(ScatterLayerArtist):
 
                         if self.state.size_mode == 'Fixed':
                             s = self.state.size * self.state.size_scaling
-                            s = broadcast_to(s, self.scatter_artist.get_sizes().shape)
+                            s = np.broadcast_to(s, self.scatter_artist.get_sizes().shape)
                         else:
                             s = ensure_numerical(self.layer[self.state.size_att].ravel())
                             if self.facet_mask is not None: