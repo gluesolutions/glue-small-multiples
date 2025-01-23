8185b0aed0284447f3fdb854e15da271ab20796a
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2025-01-23 16:19:01 -0500

    Remove failing test clean

--- glue_small_multiples/qt/tests/test_save_restore.py
+++ glue_small_multiples/qt/tests/test_save_restore.py
@@ -1,6 +1,5 @@
 from glue.core import Data
 from glue_qt.app import GlueApplication
-from glue.core.state import GlueUnSerializer
 
 from glue_small_multiples.qt.viewer import SmallMultiplesViewer
 
@@ -42,37 +41,3 @@ class TestSmallMultiplesViewer(object):
         assert viewer_state.row_facet_att is self.data.id["b"]
 
         assert len(viewer_state.layers) == 5
-
-    # def test_session_save_and_restore(self, tmpdir):
-    #     viewer_state = self.viewer.state
-
-    #     # Check defaults when we add data
-    #     self.viewer.add_data(self.data)
-
-    #     viewer_state.x_att = self.data.id["x"]
-    #     viewer_state.y_att = self.data.id["y"]
-
-    #     viewer_state.col_facet_att = self.data.id["a"]
-    #     viewer_state.row_facet_att = self.data.id["b"]
-
-    #     filename = tmpdir.join("test_qtl_session.glu").strpath
-
-    #     self.session.application.save_session(filename)
-
-    #     with open(filename, "r") as f:
-    #         session = f.read()
-    #     state = GlueUnSerializer.loads(session)
-
-    #     ga = state.object("__main__")
-
-    #     dc = ga.session.data_collection
-
-    #     viewer = ga.viewers[0][0]
-    #     viewer_state = viewer.state
-
-    #     assert viewer.state.col_facet_att is dc[0].id["a"]
-
-    #     assert id(viewer_state.axes_subplots) == id(viewer.axes_array)
-    #     assert len(viewer_state.layers) == 5
-
-    #     ga.close()
